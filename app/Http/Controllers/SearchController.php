49c7e929e18530e8a2107815cb4f6284b0444d5e
jurawrssic <juliardgs@yahoo.com.br>
2019-12-06 23:02:10 -0300

    Typeahead

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;  
+use Illuminate\Http\Request;
+use App\Models\Survivor;
+  
+class SearchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('trade');
+    }
+  
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function autocomplete(Request $request)
+    {
+        dd($data);
+        $data = Survivor::where("name","LIKE","%{$request->input('query')}%")->get();   
+        return response()->json($data);
+    }
+}
